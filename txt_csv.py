9a69268803da57286cc268afcd033c5d95838d6b
Himangi_M <himangimittal@gmail.com>
2018-06-11 22:06:25 +0530

    Final commit

--- txt_csv.py
+++ txt_csv.py
@@ -2,7 +2,12 @@
 import pandas as pd
 import numpy as np
 import csv
-for i in range(305, 314):
+import pickle as pkl
+
+with open("labels_dict.pkl", "rb") as infile:
+    labels_dict = pkl.load(infile)
+
+for i in range(302, 304):
     txt_file1 = "./dataset/USC/"+str(i)+"_P/"+str(i)+"_CLNF_features.txt"
     txt_file2 = "./dataset/USC/"+str(i)+"_P/"+str(i)+"_CLNF_features3D.txt"
     txt_file3 = "./dataset/USC/"+str(i)+"_P/"+str(i)+"_CLNF_gaze.txt"
@@ -16,8 +21,25 @@ for i in range(305, 314):
     in_txt4 = csv.reader(open(txt_file4, "r"), delimiter = ",")
     in_txt5 = csv.reader(open(txt_file5, "r"), delimiter = ",")
 
-    with open(csv_file, 'w', newline = '') as outfile:
+    with open(csv_file, 'wb') as outfile:
         out_csv = csv.writer(outfile)
+        l1 = next(in_txt1)
+        l2 = next(in_txt2)
+        l3 = next(in_txt3)
+        l4 = next(in_txt4)
+        l5 = next(in_txt5)
+
+        l1.pop(2)
+        del l2[0:4]
+        del l3[0:4]
+        del l4[0:4]
+        del l5[0:4]
+
+        combined_row = []
+        combined_row = l1 + l2 + l3 + l4 + l5
+        combined_row.append("label")
+        out_csv.writerow(combined_row)
+
         for row1, row2, row3, row4, row5 in zip(in_txt1, in_txt2, in_txt3, in_txt4, in_txt5):
             combined_row = []
             row1.pop(2)
@@ -26,6 +48,8 @@ for i in range(305, 314):
             del row4[0:4]         
             del row5[0:4]         
             combined_row = row1 + row2 + row3 + row4 + row5
+            combined_row.append(labels_dict[i])
+            print combined_row  
             out_csv.writerow(combined_row)
 
 
