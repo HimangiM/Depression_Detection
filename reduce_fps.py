b03333e4675bac51896cd7d6cc82552d985c5ea5
Himangi Mittal <himangimittal@gmail.com>
2018-06-12 14:24:34 +0530

    Update reduce_fps.py

--- reduce_fps.py
+++ reduce_fps.py
@@ -1,43 +1,48 @@
 
+
 # coding: utf-8
+
+# In[1]:
+
+
+import csv
+import os
+
 
 # In[2]:
 
 
-import csv
-
-
-# In[41]:
-
-
-_data = csv.reader(open("./frames_30fps/300_P/300_P1.csv", "r"))
-data = csv.reader(open("./frames_30fps/300_P/300_P1.csv", "r"))
-data_op = csv.writer(open("./frames_10fps/300_P/300_P1.csv", "w", newline = ''))
-
-count = int(len(list(_data)) / 3)
-
-data_op.writerow(next(data))
-# next(data)
-for row in data:
-    print ("here")
-#     print (row[i])
-    if count > 0:
-        row2 = next(data)
-        row3 = next(data)
+for j in os.listdir("./frames_30fps"):
+    for csv_file in os.listdir("./frames_30fps/" + str(j)):
         
-        l = []
-        l.append(row[0])
-        l.append(row[1])
-        l.append(row[2])
+        _data = csv.reader(open("./frames_30fps/" + str(j) + "/" + csv_file, "r"))
+        data = csv.reader(open("./frames_30fps/" + str(j) + "/" + csv_file, "r"))
+        data_op = csv.writer(open("./frames_10fps/" + str(j) + "/" + csv_file, "wb"))
         
-        for i in range(3, len(row)):
-                l.append(round(((float(row[i]) + float(row2[i]) + float(row3[i]))/3),3))  
+        print (csv_file)
+        _len = len(list(_data)) - 1
+        count = (_len / 3)
 
-        data_op.writerow(l)
-        print (l, len(l), count)
-        count = count - 1
-    else:
-        data_op.writerow(row)
-        
-        
+        data_op.writerow(next(data))
+        # next(data)
+        for row in data:
+            print ("here")
+        #     print (row[i])
+            if count > 0:
+                row2 = next(data)
+                row3 = next(data)
+
+                l = []
+                l.append(row[0])
+                l.append(row[1])
+                l.append(row[2])
+
+                for i in range(3, len(row)):
+                        l.append(round(((float(row[i]) + float(row2[i]) + float(row3[i]))/3),3))  
+
+                data_op.writerow(l)
+                print (l, len(l), count)
+                count = count - 1
+            else:
+                data_op.writerow(row)
 
